049efcaa27959b4119d574cc3276171f47b8e772
Parsons-David <david.parsons@rutgers.edu>
2017-05-31 20:46:15 -0400

    Implement Subject, Course, Section, & Meeting Objects

--- rutgers/soc.py
+++ rutgers/soc.py
@@ -33,3 +33,92 @@ def getJSON(level, campus, semester, subject):
     data = json.loads(response)
 
     return data
+
+
+# Representation of SOC Subject
+class Subject(object):
+
+    # Constructs new Subject
+    def __init__(self, number, JSON):
+
+        # TODO : Determine if the parameter is needed, can be pulled from json
+        # Sets course number (ex: "198" - CS, "640" - Math)
+        self.number = number
+        # Stores json array of courses
+        self.json = JSON
+
+        # List of the subject's course objects
+        self.courses = []
+
+        # Adds all courses in json to course list
+        for c in self.json:
+            self.courses.append(Course(c))
+
+    # Returns number of courses offered by subject
+    def numCourses(self):
+        return len(self.courses)
+
+
+# TODO : Add all JSON fields
+# Representation of SOC Course
+class Course(object):
+
+    # Constructs new Course
+    def __init__(self, JSON):
+
+        # Stores course number
+        self.number = JSON['subject']
+
+        # Stores course title
+        self.title = JSON['title']
+
+        # Stores course title
+        self.campusCode = JSON['campusCode']
+
+        # List of course's section objects
+        self.sections = []
+
+        # Adds all sections in json to section list
+        for s in JSON['sections']:
+            self.sections.append(Section(s))
+
+        # Stores json object containing Course properties
+        self.json = JSON
+
+
+# TODO : Add all JSON fields
+# Representation of SOC Section
+class Section(object):
+
+    # Constructs new Section
+    def __init__(self, JSON):
+
+        # Stores section index
+        self.index = JSON['index']
+
+        # List of section's meetings times
+        self.meetings = []
+
+        # Adds all meeting times to section list
+        for m in JSON['meetingTimes']:
+            self.meetings.append(Meeting(m))
+
+
+# TODO : Implement generateTime()
+# TODO : Add all JSON fields
+# Representation of SOC Meeting Time
+class Meeting(object):
+    # Constructs new Meeting
+    def __init__(self, JSON):
+        # Stores meeting campus
+        self.campus = JSON['campusName']
+
+        # Stores meeting building
+        self.building = JSON['buildingCode']
+
+        # Stores meeting room
+        self.room = JSON['roomNumber']
+
+        self.startTime = 0
+        self.endTime = 0
+
