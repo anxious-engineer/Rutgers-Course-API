5febf272bf5005955b8a41c2785288a5618b9166
David Parsons <david.parsons@rutgers.edu>
2018-04-03 13:28:33 -0400

    [DB-PoC] Add Parse Object

--- DB-PoC/create_db/course_parsing.py
+++ DB-PoC/create_db/course_parsing.py
@@ -5,230 +5,238 @@ from rusoc import api as soc
 import db_connect
 import json
 
-# TEST Globals
-client = db_connect.get_client()
-test_db_name = "test"
-if test_db_name in client.database_names():
-    client.drop_database(test_db_name)
-db = client[test_db_name]
+class Parser(object):
+    def __init__(self, db):
+        self.db = db
 
-# Mappings
-'''
-{
-    <collection_name_1> : {
-        "parent_keys" : [
-            "<parent_key_1>",
-            "<parent_key_2>",
-            "<parent_key_3>",
-        ],
-        "keys" : {
-            <key_name_1> : {
-                "new_key" : <new_key>,
-                "value_mappings" : {
-                    <SOC_val_1> : <NEW_API_val_1>,
-                    <SOC_val_2> : <NEW_API_val_2>,
+    # Mappings
+    '''
+    {
+        <collection_name_1> : {
+            "parent_keys" : [
+                "<parent_key_1>",
+                "<parent_key_2>",
+                "<parent_key_3>",
+            ],
+            "keys" : {
+                <key_name_1> : {
+                    "new_key" : <new_key>,
+                    "key_mod_method" : <key_mod_method_name_1>,
+                    "value_mappings" : {
+                        <SOC_val_1> : <NEW_API_val_1>,
+                        <SOC_val_2> : <NEW_API_val_2>,
+                    },
+                    "augmented_keys" : [<augmented_key_1>, <augmented_key_2>],
                 },
-                "augmented_keys" : [<augmented_key_1>, <augmented_key_2>],
-            },
-            <key_name_2> : {
-                "new_key" : <new_key>,
-                "value_mappings" : {
-                    <SOC_val_1> : <NEW_API_val_1>,
-                    <SOC_val_2> : <NEW_API_val_2>,
+                <key_name_2> : {
+                    "new_key" : <new_key>,
+                    "key_mod_method" : <key_mod_method_name_1>,
+                    "value_mappings" : {
+                        <SOC_val_1> : <NEW_API_val_1>,
+                        <SOC_val_2> : <NEW_API_val_2>,
+                    },
+                    "augmented_keys" : [<augmented_key_1>, <augmented_key_2>],
                 },
-                "augmented_keys" : [<augmented_key_1>, <augmented_key_2>],
             },
         },
-    },
-    ...
-}
-'''
-#   - Collection Name *Required*
-#     - Parent Keys (Location of key inside nested SOC JSON)
-#       *Key is only present if they are required*
-#       *Absent Key is equivalent to 'parent_keys : None'*
-#       *In order they will be counter in tree/dict*
-#     - Keys (SOC -> New API) *SHOULD NEVER BE NONE*
-#       *Value is only present if they are required*
-#       *Absent Value is equivalent to '"new_key" : None,"value_mappings" : None'*
-#         - Name (Data Name inside SOC JSON)
-#         - Mapping (New Mapped Name of Data)
-#          *Key is only present if they are required*
-#           *Absent Key is equivalent to 'value_mappings : None'*
-#         - Augmented Keys (Additional names for this key)
-
-# NOTE : Keys that have values that are Arrays instead of maps,
-# Will be handled implicitly by the parser, which will test 'isinstance(val, list)'
-collection_mappings = {
-    "course" : {
-        "keys" : {
-            "courseNumber" : {
-                "new_key" : "number",
-            },
-            "courseNotes" : {
-                "new_key" : "notes",
-            },
-            "courseDescription" : {
-                "new_key" : "description",
-            },
-            "synopsisUrl" : None,
-            "title" : None,
-            "preReqNotes" : None,
-            "credits" : None,
-            "expandedTitle" : None,
-        }
-    },
-    "subject" : {
-        "keys" : {
-            "subject" : {
-                "new_key" : "number"
-            },
-        }
-    },
-    "professor" : {
-        "parent_keys" : ["sections", "instructors"],
-        "keys" : {
-            "name" : {
-                "new_key" : "name",
-                "key_mod_method" : "get_prof_last_name",
-            },
-        }
-    },
-    "campus" : {
-        "keys" : {
-            "campusCode" : {
-                "new_key" : "name",
-                "value_mappings" : {
-                    'NB' : 'New Brunswick',
-                    'NK' : 'Newark',
-                    'CM' : 'Camden',
-                    'OC' : 'Off Campus',
-                    'ON' : 'Online'
-                },
-                "augmented_keys" : ["code"]
-            },
-        },
+        ...
     }
-}
+    '''
+    #   - Collection Name *Required*
+    #     - Parent Keys (Location of key inside nested SOC JSON)
+    #       *Key is only present if they are required*
+    #       *Absent Key is equivalent to 'parent_keys : None'*
+    #       *In order they will be counter in tree/dict*
+    #     - Keys (SOC -> New API) *SHOULD NEVER BE NONE*
+    #       *Value is only present if they are required*
+    #       *Absent Value is equivalent to '"new_key" : None,"value_mappings" : None'*
+    #         - Name (Data Name inside SOC JSON)
+    #         - Key Mod Method (Method name that properly formats key data)
+    #         - Mapping (New Mapped Name of Data)
+    #          *Key is only present if they are required*
+    #           *Absent Key is equivalent to 'value_mappings : None'*
+    #         - Augmented Keys (Additional names for this key)
 
+    # NOTE : Keys that have values that are Arrays instead of maps,
+    # Will be handled implicitly by the parser, which will test 'isinstance(val, list)'
+    collection_mappings = {
+        "course" : {
+            "keys" : {
+                "courseNumber" : {
+                    "new_key" : "number",
+                },
+                "courseNotes" : {
+                    "new_key" : "notes",
+                },
+                "courseDescription" : {
+                    "new_key" : "description",
+                },
+                "synopsisUrl" : None,
+                "title" : None,
+                "preReqNotes" : None,
+                "credits" : None,
+                "expandedTitle" : None,
+            }
+        },
+        "subject" : {
+            "keys" : {
+                "subject" : {
+                    "new_key" : "number"
+                },
+            }
+        },
+        "professor" : {
+            "parent_keys" : ["sections", "instructors"],
+            "keys" : {
+                "name" : {
+                    "new_key" : "name",
+                    "key_mod_method" : "get_prof_last_name",
+                },
+            }
+        },
+        "campus" : {
+            "keys" : {
+                "campusCode" : {
+                    "new_key" : "name",
+                    "value_mappings" : {
+                        'NB' : 'New Brunswick',
+                        'NK' : 'Newark',
+                        'CM' : 'Camden',
+                        'OC' : 'Off Campus',
+                        'ON' : 'Online'
+                    },
+                    "augmented_keys" : ["code"]
+                },
+            },
+        }
+    }
 
-# Mod methods
-def get_prof_last_name(key):
-    if "," in key:
-        return key.split(",")[0]
-    return key
+    # Mod methods
+    def get_prof_last_name(key):
+        if "," in key:
+            return key.split(",")[0]
+        return key
 
-def do_nothing(key):
-    return key
+    def do_nothing(key):
+        return key
 
-MOD_METHODS = {
-    "get_prof_last_name" : get_prof_last_name
-}
+    MOD_METHODS = {
+        "get_prof_last_name" : get_prof_last_name
+    }
 
-def parse_course_data(all_data):
-    all_new_course_data = {}
-    for name in collection_mappings:
-        all_new_course_data[name] = []
+    def parse_course_data(self, all_data):
+        all_new_course_data = {}
+        for name in Parser.collection_mappings:
+            all_new_course_data[name] = []
 
-    for course in all_data:
-        print(course['courseNumber'])
-        local_refs = {}
-        for name, c_mapping in collection_mappings.items():
-            local_refs[name] = []
-            # Current data we parse is always starts as just the current course json
-            current_data = [course]
-            # print("\t" + name)
-            # Do we need to dive in?
-            # Here we adjust current data based on parents
-            if c_mapping.get('parent_keys'):
-                for p_key in c_mapping['parent_keys']:
-                    # print("\t*%s*" % p_key)
-                    tmp_data = []
-                    for data in current_data:
-                        if isinstance(data[p_key], list):
-                            for sub_data in data[p_key]:
-                                tmp_data.append(sub_data)
+        for course in all_data:
+            print(course['courseNumber'])
+            local_refs = {}
+            for name, c_mapping in Parser.collection_mappings.items():
+                local_refs[name] = []
+                # Current data we parse is always starts as just the current course json
+                current_data = [course]
+                # print("\t" + name)
+                # Do we need to dive in?
+                # Here we adjust current data based on parents
+                if c_mapping.get('parent_keys'):
+                    for p_key in c_mapping['parent_keys']:
+                        # print("\t*%s*" % p_key)
+                        tmp_data = []
+                        for data in current_data:
+                            if isinstance(data[p_key], list):
+                                for sub_data in data[p_key]:
+                                    tmp_data.append(sub_data)
+                            else:
+                                tmp_data.append(data[p_key])
+                        current_data = list(tmp_data)
+                # print(current_data)
+                for data in current_data:
+                    new_doc = {}
+                    for key in c_mapping['keys']:
+                        name_update_mapping = c_mapping['keys'].get(key)
+                        if name_update_mapping:
+                            new_key = key if not name_update_mapping.get('new_key') else name_update_mapping['new_key']
+                            output_val = data[key] if not name_update_mapping.get('value_mappings') else name_update_mapping['value_mappings'].get(data[key])
+                            output_val = Parser.MOD_METHODS.get(name_update_mapping.get('key_mod_method', "None"), Parser.do_nothing)(output_val)
+                            new_doc[new_key] = output_val
+                            # print("\t\t %s : %s" % (new_key, output_val))
+                            if name_update_mapping.get('augmented_keys'):
+                                for augmented_key in name_update_mapping.get('augmented_keys'):
+                                    new_doc[augmented_key] = data[key]
+                                    # print("\t\t %s : %s" % (augmented_key, data[key]))
                         else:
-                            tmp_data.append(data[p_key])
-                    current_data = list(tmp_data)
-            # print(current_data)
-            for data in current_data:
-                new_doc = {}
-                for key in c_mapping['keys']:
-                    name_update_mapping = c_mapping['keys'].get(key)
-                    if name_update_mapping:
-                        new_key = key if not name_update_mapping.get('new_key') else name_update_mapping['new_key']
-                        output_val = data[key] if not name_update_mapping.get('value_mappings') else name_update_mapping['value_mappings'].get(data[key])
-                        output_val = MOD_METHODS.get(name_update_mapping.get('key_mod_method', "None"), do_nothing)(output_val)
-                        new_doc[new_key] = output_val
-                        # print("\t\t %s : %s" % (new_key, output_val))
-                        if name_update_mapping.get('augmented_keys'):
-                            for augmented_key in name_update_mapping.get('augmented_keys'):
-                                new_doc[augmented_key] = data[key]
-                                # print("\t\t %s : %s" % (augmented_key, data[key]))
-                    else:
-                        new_doc[key] = data[key]
-                        # print("\t\t %s : %s" % (key, data[key]))
-                local_refs[name].append(new_doc)
-        push_course_data_to_db(local_refs)
+                            new_doc[key] = data[key]
+                            # print("\t\t %s : %s" % (key, data[key]))
+                    local_refs[name].append(new_doc)
+            self.push_course_data_to_db(local_refs)
 
-def update_references(relatives):
-    # print("RELATIVES: %s" % relatives)
-    for coll, v in relatives.items():
-        # Copy Dict
-        refs = relatives.copy()
-        # Remove coll
-        refs.pop(coll, None)
-        if isinstance(v, list):
-            for i in v:
-                upsert_references(i, coll, refs)
-        else:
-            upsert_references(i, coll, refs)
+    def update_references(self, relatives):
+        # print("RELATIVES: %s" % relatives)
+        for coll, v in relatives.items():
+            # Copy Dict
+            refs = relatives.copy()
+            # Remove coll
+            refs.pop(coll, None)
+            if isinstance(v, list):
+                for i in v:
+                    self.upsert_references(i, coll, refs)
+            else:
+                self.upsert_references(i, coll, refs)
 
-def upsert_references(d_id, coll, refs):
-    doc = db[coll].find_one({"_id" : d_id})
-    if not doc:
-        print('Failure Finding doc in %s in upsert_references : %s' % (coll, d_id))
-    db[coll].update(
-        {"_id" : d_id},
-        {"$set" : merge_references(refs, doc)}
-        )
+    def upsert_references(self, d_id, coll, refs):
+        doc = self.db[coll].find_one({"_id" : d_id})
+        if not doc:
+            print('Failure Finding doc in %s in upsert_references : %s' % (coll, d_id))
+        self.db[coll].update(
+            {"_id" : d_id},
+            {"$set" : self.merge_references(refs, doc)}
+            )
 
-def merge_references(new_refs, old_doc):
-    # print(new_refs)
-    # print(old_doc)
-    # update new_refs name
-    new_db_refs = {}
-    for ref_name in new_refs:
-        new_db_refs[('__%s__' % ref_name)] = list(new_refs[ref_name])
-    output_refs = {}
-    for ref_name in new_db_refs:
-        output_refs[ref_name] = old_doc.get(ref_name, []) + list(set(new_db_refs.get(ref_name, [])) - set(old_doc.get(ref_name, [])))
-    return output_refs
+    def merge_references(self, new_refs, old_doc):
+        # print(new_refs)
+        # print(old_doc)
+        # update new_refs name
+        new_db_refs = {}
+        for ref_name in new_refs:
+            new_db_refs[('__%s__' % ref_name)] = list(new_refs[ref_name])
+        output_refs = {}
+        for ref_name in new_db_refs:
+            output_refs[ref_name] = old_doc.get(ref_name, []) + list(set(new_db_refs.get(ref_name, [])) - set(old_doc.get(ref_name, [])))
+        return output_refs
 
-def push_course_data_to_db(local_refs):
-    db_refs = {}
-    # ('__%s__' % name)
-    # print(json.dumps(local_refs, indent=4))
-    # Build DB refs
-    for coll in local_refs:
-        db_refs[coll] = []
-        for doc in local_refs[coll]:
-            db_refs[coll].append(upsert_doc(doc, coll))
-    update_references(db_refs)
-    # print(json.dumps(db_refs, indent=4))
+    def push_course_data_to_db(self, local_refs):
+        db_refs = {}
+        # ('__%s__' % name)
+        # print(json.dumps(local_refs, indent=4))
+        # Build DB refs
+        for coll in local_refs:
+            db_refs[coll] = []
+            for doc in local_refs[coll]:
+                db_refs[coll].append(self.upsert_doc(doc, coll))
+        self.update_references(db_refs)
+        # print(json.dumps(db_refs, indent=4))
 
-# Returns object id of document that either already exists or is created
-def upsert_doc(doc, coll_name):
-    collection = db[coll_name]
-    tar = collection.count(doc)
-    if tar == 0:
-        collection.insert(doc)
-    tar = collection.find_one(doc)
-    return tar["_id"]
+    # Returns object id of document that either already exists or is created
+    def upsert_doc(self, doc, coll_name):
+        collection = self.db[coll_name]
+        tar = collection.count(doc)
+        if tar == 0:
+            collection.insert(doc)
+        tar = collection.find_one(doc)
+        return tar["_id"]
 
 # Test Parsing Code
 def test_course_parsing():
+    # TEST
+    client = db_connect.get_client()
+    test_db_name = "test"
+    if test_db_name in client.database_names():
+        client.drop_database(test_db_name)
+    db = client[test_db_name]
+    # Parser should take a DB object on creation
+    p = Parser(db)
+    # Then Parse data
     test_data = []
     all_urls = soc.get_all_current_course_urls()
     i = 0
@@ -239,15 +247,10 @@ def test_course_parsing():
     # Now we've got come clean data!
     # Parse it!
     # print(test_data)
-    parse_course_data(test_data)
+    p.parse_course_data(test_data)
 
 # Entry to Script for parsing test
 def main():
-    if not client:
-        print("Error with Client: %s" % client)
-    if not db:
-        print("Error with db: %s" % db)
-    print(db)
     test_course_parsing()
 
 if __name__ == '__main__':
--- DB-PoC/create_db/create_db.py
+++ DB-PoC/create_db/create_db.py
@@ -5,15 +5,8 @@ from rusoc import api as soc
 import pymongo
 import argparse
 import urllib.request, json
-
-# CONSTANTS FOR PoC
-PoC_SEMESTER = "12018"
-PoC_CAMPUSES = ['NB', 'NK', 'CM', 'WM', 'AC', 'MC', 'J', 'RV', 'CC', 'CU']
-PoC_LEVELS = ["UG", "G"]
-
-# TODO : HIDE & MODULARIZE
-PASSWORD = "makingausableapi"
-uri = "mongodb+srv://admin:" + PASSWORD + "@poc-5vbvd.mongodb.net/test"
+import course_parsing
+import db_connect
 
 def get_args():
     # parse all the arguments to the client
@@ -24,194 +17,6 @@ def get_args():
     args = vars(parser.parse_args())
     return args
 
-def extract_course(d):
-    keys = [
-    "courseNumber",
-    "synopsisUrl",
-    "title",
-    "courseDescription",
-    "preReqNotes",
-    "credits",
-    "courseNotes",
-    "expandedTitle"]
-
-    key_map = {
-        "courseNumber" : "number",
-        "courseDescription" : "description",
-        "courseNotes" : "notes",
-    }
-
-    new_doc = {}
-    for key in keys:
-        if d[key]:
-            new_key = key_map.get(key) if key_map.get(key) else key
-            new_doc[new_key] = d[key]
-
-    return new_doc
-
-
-def extract_subject(d):
-    keys = ["subject"]
-
-    key_map = {
-        "subject" : "number",
-    }
-
-    new_doc = {}
-    for key in keys:
-        if d[key]:
-            new_key = key_map.get(key) if key_map.get(key) else key
-            new_doc[new_key] = d[key]
-
-    return new_doc
-
-def parse_professor(data):
-    keys = ["name"]
-
-    new_docs = []
-
-    for d in data:
-        if not d['sections']:
-            continue
-        secs = d['sections']
-        for sec in secs:
-            if not sec['instructors']:
-                continue
-            for ins in sec['instructors']:
-                new_doc = {}
-                for key in keys:
-                    if ins[key]:
-                        new_doc[key] = ins[key]
-
-                if not new_doc is {}:
-                    new_docs.append(new_doc)
-
-    return new_docs
-
-def parse_campus(data):
-    keys = ["campusCode"]
-    key_map = {
-        'campusCode' : 'code'
-    }
-    code_to_name = {
-        'NB' : 'New Brunswick',
-        'NK' : 'Newark',
-        'CM' : 'Camden',
-        'OC' : 'Off Campus',
-        'ON' : 'Online'
-    }
-
-    new_docs = []
-
-    for d in data:
-        new_doc = {}
-        for key in keys:
-            if d[key]:
-                new_key = key_map.get(key) if key_map.get(key) else key
-                new_doc[new_key] = d[key]
-                if key == 'campusCode':
-                    new_key = 'name'
-                    if code_to_name.get(d[key]):
-                        new_doc[new_key] = code_to_name.get(d[key])
-
-        if not new_doc is {}:
-            new_docs.append(new_doc)
-
-    return new_docs
-
-def upsert(collection, doc):
-    tar = collection.count(doc)
-    if tar == 0:
-        collection.insert(doc)
-        return
-    tar = collection.find(doc)[0]
-    return {"_id" : tar["_id"]}
-
-# POPULATES a database with the JSON Data
-def populate_database(client, db):
-    # Gather Data
-
-    all_urls = soc.get_all_current_course_urls()
-
-    attempts = 1
-
-    fails = open('fails', 'w')
-
-    while attempts > 0:
-        print("Attempts Remaing %d" % attempts)
-        count = 0
-        failed = 0
-        total = len(all_urls)
-        failures = []
-        for url in all_urls:
-            print("\r%d of %d Complete | %d Failures | Current -> %s" % (count, total, failed, url), end='')
-            # GET DATA
-            data = soc.get_clean_JSON(url)
-
-            if len(data) == 0:
-                failed += 1
-                fails.writelines(url + '\n')
-                failures.append(url)
-            else:
-                # Parse data
-                for c in data:
-                    references = {
-                        "__professor__" : [],
-                        "__campus__" : [],
-                        "__subject__" : "",
-                        "__course__" : []
-                    }
-                    # Get and insert Subject
-                    sub_doc = extract_subject(c)
-                    if not sub_doc:
-                        continue
-                    # Add subject if new
-                    sub_doc = upsert(db['subject'], sub_doc)
-                    references['__subject__'] = sub_doc
-                    # Get and insert course
-                    c_doc = extract_course(c)
-                    if not c_doc:
-                        continue
-                    c_doc = upsert(db['course'], c_doc)
-                    references['__course__'] = c_doc
-
-                    # Get and insert Professor
-                    # Get and insert Campus
-                    # Update References
-                    update_references(references)
-                professors = parse_professor(data)
-                if len(professors) > 0:
-                    db['professor'].insert(professors)
-                # else:
-                #     print('\r' + url)
-
-                campuses = parse_campus(data)
-                if len(campuses) > 0:
-                    db['campus'].insert(campuses)
-                # else:
-                #     print('\r' + url)
-            count+=1
-            # print("\tPushed %d professors" % len(professors))
-        all_urls = failures
-        attempts -= 1
-
-    print(all_urls)
-    fails.close()
-
-
-def attempt_creation(client, db_name):
-    # Create PoC DB if neccesary
-    if db_name in client.database_names():
-        print("%s DB exists!" % (db_name))
-        return
-
-    # PoC Database Object
-    PoC_DB = client[db_name]
-
-    populate_database(client, PoC_DB)
-
-    print(PoC_DB.collection_names())
-
 def main():
     # Get CLI Args passed to script
     cli_args = get_args()
@@ -219,9 +24,20 @@ def main():
     db_name = str(cli_args['name'])
 
     # Connect to Mongo Cluster
-    client = pymongo.MongoClient(uri)
+    client = db_connect.get_client()
 
-    attempt_creation(client, db_name)
+    # PoC Database Object
+    DB = client[db_name]
+
+    # Create a DB object
+    # Create parser object with DB object
+    # Parse all courses into it
+
+    parser = course_parsing.Parser(DB) # Parser(DB)
+    all_urls = soc.get_all_current_course_urls()
+    for url in all_urls:
+        data = soc.get_clean_JSON(url)
+        parser.parse_course_data(data)
 
     # Close Mongo Connection
     close_response = client.close()
